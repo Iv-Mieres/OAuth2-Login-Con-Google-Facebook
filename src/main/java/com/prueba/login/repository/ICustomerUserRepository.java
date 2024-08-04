4c319cd8453aa8aa9c5a30d1683a59f5050bda3a
Ivan Mieres <mieres.iv@gmail.com>
2024-08-04 20:41:50 -0300

    fix: CustomerUserRepository

--- src/main/java/com/prueba/login/repository/ICustomerUserRepository.java
+++ src/main/java/com/prueba/login/repository/ICustomerUserRepository.java
@@ -9,8 +9,8 @@ import java.util.Optional;
 @Repository
 public interface ICustomerUserRepository extends JpaRepository<CustomerUser, Long> {
     Optional<CustomerUser> findByEmail(String email);
-    Optional<CustomerUser> findBySocialIdAndProvider(String id, String provider);
+
     Boolean existsByEmail(String email);
-    Boolean existsByEmailAnId(String email, Long id);
+
 
 }
